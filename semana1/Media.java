b78ddbedbd77b4b62d9c495c8a7724b6f5c4f115
duda_cardoso <126731364+dudajosecardoso@users.noreply.github.com>
2024-08-12 20:57:50 -0300

    Create Media.java

---
+++ semana1/Media.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Media{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a sua primeira nota: ");
+        double nota1 = scanner.nextDouble();
+        System.out.println("Digite a sua segunda nota:");
+        double nota2 = scanner.nextDouble();
+        System.out.println("Digite a sua terceira nota:");
+        double nota3 = scanner.nextDouble();
+        double mediaFinal = (nota1 + nota2 + nota3) / 3;
+        System.out.println("A média final é: " + mediaFinal);
+        scanner.close();
+    }
+}
