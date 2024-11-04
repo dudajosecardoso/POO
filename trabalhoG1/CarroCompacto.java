f2816bf16dab0a83fd542934efb7888e56c1502e
duda_cardoso <126731364+dudajosecardoso@users.noreply.github.com>
2024-11-03 23:13:51 -0300

    Create CarroCompacto.java

---
+++ trabalhoG1/CarroCompacto.java
@@ -0,0 +1,6 @@
+//Classe Filha da classe CarroEletrico
+public class CarroCompacto extends CarroEletrico {
+    public CarroCompacto(String id, String marca, String modelo, int anoFabricacao, double capacidadeBateria) {
+        super(id, marca, modelo, anoFabricacao, capacidadeBateria, 200);
+    }
+}
