1dd21c7868dac436952946f983a918de11890198
duda_cardoso <126731364+dudajosecardoso@users.noreply.github.com>
2024-11-03 23:13:06 -0300

    Create CarroSedan.java

---
+++ trabalhoG1/CarroSedan.java
@@ -0,0 +1,6 @@
+//Classe Filha da classe CarroEletrico
+public class CarroSedan extends CarroEletrico {
+    public CarroSedan(String id, String marca, String modelo, int anoFabricacao, double capacidadeBateria) {
+        super(id, marca, modelo, anoFabricacao, capacidadeBateria, 400);
+    }
+}
