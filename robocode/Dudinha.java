bb748655b13e610f7dc05b5726c1a250a11edf67
duda_cardoso <126731364+dudajosecardoso@users.noreply.github.com>
2024-11-27 21:26:36 -0300

    Update Dudinha.java

--- robocode/Dudinha.java
+++ robocode/Dudinha.java
@@ -2,8 +2,11 @@ package Dudinha;
 
 import robocode.*;
 
-public class Dudinha extends Robot { //HERANÇA
-    public void run() { // METODO
+// Classe que representa o robô Dudinha, que herda da classe Robot do Robocode
+public class Dudinha extends Robot {
+    
+    // Método principal que define o comportamento do robô
+    public void run() {
         while (true) {
             ahead(100);
             turnGunRight(360);
@@ -13,10 +16,11 @@ public class Dudinha extends Robot { //HERANÇA
         }
     }
 
-    public void onScannedRobot(ScannedRobotEvent e) { //METODO
-        double enemyBearing = e.getBearing();
-        double enemyHeading = e.getHeading();
-        double enemyVelocity = e.getVelocity();
+    // Método chamado quando o robô escaneia outro robô
+    public void onScannedRobot(ScannedRobotEvent e) {
+        double enemyBearing = e.getBearing(); 
+        double enemyHeading = e.getHeading(); 
+        double enemyVelocity = e.getVelocity(); 
         double futureX = getX() + Math.sin(Math.toRadians(enemyHeading)) * enemyVelocity;
         double futureY = getY() + Math.cos(Math.toRadians(enemyHeading)) * enemyVelocity;
         double radarTurn = enemyBearing; 
@@ -28,16 +32,16 @@ public class Dudinha extends Robot { //HERANÇA
         fire(1);
     }
 
-    public void onHitByBullet(HitByBulletEvent e) { //METODO
+    // Método chamado quando o robô é atingido por uma bala
+    public void onHitByBullet(HitByBulletEvent e) {
         back(50);
         turnRight(90);
     }
 
-    public void onHitWall(HitWallEvent e) { // METODO
+    // Método chamado quando o robô colide com uma parede
+    public void onHitWall(HitWallEvent e) {
         back(20);
         turnRight(90);
     }
 
-    public void onRobotDeath(RobotDeathEvent e) { //METODO
-    }
 }
